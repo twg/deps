e5e136ce2f09668adf7d580f8b05d94a4fb32fad
Will Langstroth <will@langstroth.com>
2013-03-22 14:20:02 -0400

    fix deploy user dep

--- deploy_user.rb
+++ deploy_user.rb
@@ -1,13 +1,19 @@
-dep "deploy user.task" do
+dep "deploy user" do
   requires "web directory"
 
-  shell "groupadd deploy"
-  shell "useradd deploy -g deploy"
-  # usermod -G wheel deploy
-  shell "mkdir /home/deploy/.ssh"
-  shell "touch /home/deploy/.ssh/config"
-  shell "touch /home/deploy/.ssh/authorized_keys"
-  shell "chmod go-rwx /home/deploy/.ssh/*"
-  shell "chown -R deploy:deploy /home/deploy/.ssh"
-  shell "# chown -R deploy:deploy /web"
+  met? {
+    "/etc/passwd".p.grep("deploy")
+  }
+
+  meet {
+    shell "groupadd deploy"
+    shell "useradd deploy -g deploy"
+    # usermod -G wheel deploy
+    shell "mkdir /home/deploy/.ssh"
+    shell "touch /home/deploy/.ssh/config"
+    shell "touch /home/deploy/.ssh/authorized_keys"
+    shell "chmod go-rwx /home/deploy/.ssh/*"
+    shell "chown -R deploy:deploy /home/deploy/.ssh"
+    shell "# chown -R deploy:deploy /web"
+  }
 end
