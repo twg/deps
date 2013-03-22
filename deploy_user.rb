11bf2d2ec6fa6eca0f31ae5f7b9d62f87109e249
Will Langstroth <will@langstroth.com>
2013-03-22 14:41:46 -0400

    test for ownership

--- deploy_user.rb
+++ deploy_user.rb
@@ -1,6 +1,7 @@
 dep "deploy user" do
   requires "web directory"
   requires "deploy group"
+  requires "deploy user owns web"
 
   met? {
     "/etc/passwd".p.grep(/^deploy/)
@@ -14,7 +15,6 @@ dep "deploy user" do
     shell "touch /home/deploy/.ssh/authorized_keys"
     shell "chmod go-rwx /home/deploy/.ssh/*"
     shell "chown -R deploy:deploy /home/deploy/.ssh"
-    shell "# chown -R deploy:deploy /web"
   }
 end
 
@@ -24,5 +24,15 @@ dep "deploy group" do
   }
   meet {
     shell "groupadd deploy"
+  }
+end
+
+dep "deploy user owns web" do
+  met? {
+    # FIXME: This is a weak test
+    shell "ls -l / | grep 'deploy deploy'"
+  }
+  meet {
+    shell "chown -R deploy:deploy /web"
   }
 end
