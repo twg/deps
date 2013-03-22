b7452dcbddb45c9640d3b398c67058f08698d055
Will Langstroth <will@langstroth.com>
2013-03-22 08:56:32 -0400

    reformat requires list

--- daemons.rb
+++ daemons.rb
@@ -1,7 +1,12 @@
 dep "daemons disabled" do
-  requires %w( netfs nfslock rpcbind rpcgssd rpcidmapd sendmail ).map { |d|
-    "#{d}.disabled_daemon"
-  }
+  requires %w(
+    netfs
+    nfslock
+    rpcbind
+    rpcgssd
+    rpcidmapd
+    sendmail
+  ).map { |d| "#{d}.disabled_daemon" }
 end
 
 dep "netfs.disabled_daemon"
