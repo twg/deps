e242aaf7667ddd0b3e8a915207dc49a50f04167b
Will Langstroth <will@langstroth.com>
2013-03-29 23:08:26 -0400

    add note

--- selinux.rb
+++ selinux.rb
@@ -1,5 +1,6 @@
 dep "selinux disabled", :for => :linux do
   requires "perl.bin"
+  # TODO: add met? section
   meet {
     log_shell "Disable selinux", "perl -pi -e 's/enforcing/disabled/' /etc/selinux/config"
   }
