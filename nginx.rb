354d96e97436013e42c27429d2400fb30dcd1f85
Will Langstroth <will@langstroth.com>
2013-05-14 11:50:41 -0400

    add configured nginx stub

--- nginx.rb
+++ nginx.rb
@@ -1,3 +1,11 @@
-dep "nginx" do
+dep "nginx.bin"
 
+dep 'configured.nginx', :nginx_prefix do
+  requires 'nginx.bin'
+  met? {
+    Babushka::Renderable.new(nginx_conf).from?(dependency.load_path.parent / "templates/nginx.conf.erb")
+  }
+  meet {
+    render_erb 'templates/nginx.conf.erb', :to => nginx_conf, :sudo => true
+  }
 end
