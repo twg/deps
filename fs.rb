69afb2b1267bd570d1a9e176a5707347dc981162
Will Langstroth <will@langstroth.com>
2013-06-15 12:35:06 -0400

    add admin group handling

--- fs.rb
+++ fs.rb
@@ -21,3 +21,14 @@ dep 'layout.fs', :path do
   met? { subpaths.all? {|subpath| File.directory?(path / subpath) } }
   meet { sudo "mkdir -p #{subpaths.map {|subpath| "'#{path / subpath}'" }.join(' ')}" }
 end
+
+dep 'admins can sudo' do
+  requires 'admin group', 'sudo.bin'
+  met? { !sudo('cat /etc/sudoers').split("\n").grep(/^%admin/).empty? }
+  meet { append_to_file '%admin  ALL=(ALL) ALL', '/etc/sudoers', :sudo => true }
+end
+
+dep 'admin group' do
+  met? { '/etc/group'.p.grep(/^admin\:/) }
+  meet { sudo 'groupadd admin' }
+end
