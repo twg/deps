65d2cceb565a7985bc294144e5a443ee8e68065e
Will Langstroth <will@langstroth.com>
2013-06-27 13:30:21 -0400

    add personal laptop config

---
+++ will.rb
@@ -0,0 +1,13 @@
+dep "laptop" do
+  requires [
+    "ruby",
+    "rubygems",
+    "vim config",
+    "libraries",
+    "ruby-build",
+    "2.0.0.rbenv",
+    "1.9.3.rbenv",
+    "powder.gem",
+    "apps"
+  ]
+end
